865159f93e4ceeea7bbf8d5504801bca22526572
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2020-04-24 16:20:58 +0100

    Backtacking

--- src/main/scala/fr/damienraymond/sit/domain/service/change/MyersExperimentation.scala
+++ src/main/scala/fr/damienraymond/sit/domain/service/change/MyersExperimentation.scala
@@ -277,8 +277,11 @@ object MyersExperimentation {
 
   type Coord = (Int, Int)
 
-  def diff(original: SortedMap[Int, String], newVersion: SortedMap[Int, String]): Map[Int, Int] = {
+  type Depth = Int
+  type V = Map[Int, Int]
+  type History = SortedMap[Depth, V]
 
+  def change(original: SortedMap[Int, String], newVersion: SortedMap[Int, String]) = {
     val `|original|` = original.size
     val `|newVersion|` = newVersion.size
 
@@ -286,56 +289,110 @@ object MyersExperimentation {
 
     val expectedCoord = (`|original|`, `|newVersion|`)
 
-    @scala.annotation.tailrec
-    def loop(coord: Coord, acc: Map[Int, Int], depth: Int): Map[Int, Int] =
-      if(depth <= max && coord != expectedCoord) {
-        val (nCoord, nAcc) = loopDepth(coord, acc, depth, -depth)
-        loop(nCoord, nAcc, depth + 1)
-      } else acc
+
+    def diff: History = {
 
 
-    @scala.annotation.tailrec
-    def loopDepth(coord: Coord, acc: Map[Int, Int], depth: Int, k: Int): (Coord, Map[Int, Int]) =
-      if(k <= depth && coord != expectedCoord) {
-        val (nCoord, nAcc) = loopK(depth)(acc, k)
-        loopDepth(nCoord, nAcc, depth, k + 2)
-      } else (coord, acc)
+      @scala.annotation.tailrec
+      def loop(coord: Coord, acc: V, history: History, depth: Int): History =
+        if (depth <= max && coord != expectedCoord) {
+          val (nCoord, nAcc) = loopDepth(coord, acc, depth, -depth)
+          loop(nCoord, nAcc, history.updated(depth, nAcc), depth + 1)
+        } else history
 
 
-    def loopK(depth: Int)(acc: Map[Int, Int], k: Int): (Coord, Map[Int, Int]) = {
+      @scala.annotation.tailrec
+      def loopDepth(coord: Coord, acc: V, depth: Int, k: Int): (Coord, V) =
+        if (k <= depth && coord != expectedCoord) {
+          val (nCoord, nAcc) = loopK(depth)(acc, k)
+          loopDepth(nCoord, nAcc, depth, k + 2)
+        } else (coord, acc)
 
-      val x = {
-        if (k == -depth || (k != depth && acc(k - 1) < acc(k + 1)))
-          acc(k + 1)
+
+      def loopK(depth: Int)(acc: V, k: Int): (Coord, V) = {
+
+        val x = {
+          if (k == -depth || (k != depth && acc(k - 1) < acc(k + 1)))
+            acc(k + 1)
+          else
+            acc(k - 1) + 1
+        }
+
+        val y = x - k
+
+        val coord@(nx, ny) = loopDiagonal(acc)(x, y)
+
+        val res = acc.updated(k, nx)
+
+        if (nx >= `|original|` && ny >= `|newVersion|`) {
+          println(res)
+        }
+
+        (coord, res)
+
+      }
+
+      @scala.annotation.tailrec
+      def loopDiagonal(acc: V)(x: Int, y: Int): (Int, Int) =
+        if (x < `|original|` && y < `|newVersion|` && original(x) == newVersion(y))
+          loopDiagonal(acc)(x + 1, y + 1)
         else
-          acc(k - 1) + 1
+          (x, y)
+
+
+      loop((0, 0), Map(1 -> 0), SortedMap.empty, 0)
+    }
+
+    val history = diff
+
+
+    val maxDepth = history.keys.max
+
+
+    type Trace = List[Coord]
+
+    def backtrack(history: List[(Depth, V)]): Trace = {
+
+      @scala.annotation.tailrec
+      def loopDiagonal(trace: Trace)(x: Int, y: Int): Trace =
+        if (x > expectedCoord._1 && y > expectedCoord._2)
+          loopDiagonal((x - 1, y - 1) :: trace)(x - 1, y - 1)
+        else
+          trace
+
+
+      @scala.annotation.tailrec
+      def loop(history: List[(Depth, V)])(trace: Trace)(x: Int, y: Int): Trace = history match {
+        case (depth, acc) :: next =>
+
+          val k = x - y
+
+          val prevK =
+            if (k == -depth || (k != depth && acc(k - 1) < acc(k + 1))) {
+              k + 1
+            } else {
+              k - 1
+            }
+
+          val prevX = acc(prevK)
+          val prevY = prevX - prevK
+
+
+          val nTrace = loopDiagonal(trace)(x, y)
+
+          loop(next)((x, y) :: nTrace)(prevX, prevY)
+
+        case Nil =>
+          trace
       }
 
-      val y = x - k
-
-
-      val coord @ (nx, ny) = loopDiagonal(acc)(x, y)
-
-      val res = acc.updated(k, nx)
-
-      if(nx >= `|original|` && ny >= `|newVersion|`){
-        println(res)
-      }
-
-      (coord, res)
+      loop(history)(List.empty)(expectedCoord._1, expectedCoord._2)
 
     }
 
-    @scala.annotation.tailrec
-    def loopDiagonal(acc: Map[Int, Int])(x: Int, y: Int): (Int, Int) =
-      if(x < `|original|` && y < `|newVersion|` && original(x) == newVersion(y))
-        loopDiagonal(acc)(x + 1, y + 1)
-      else
-        (x, y)
 
+    backtrack(diff.toList.reverse)
 
-
-    loop((0,0), Map(1 -> 0), 0)
   }
 
 
@@ -344,7 +401,7 @@ object MyersExperimentation {
     val original = SortedMap.from(LazyList.from(0).zip("abcabba".split("")))
     val newVersion = SortedMap.from(LazyList.from(0).zip("cbabac".split("")))
 
-    println(diff(original, newVersion).toList.sortBy(_._1).map(_._2))
+    println(change(original, newVersion))
   }
 
 
