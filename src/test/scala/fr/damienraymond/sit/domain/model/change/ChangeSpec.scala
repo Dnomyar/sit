97fe048a24b75f2c7b53d64f338a3762c4eedd65
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2020-04-29 11:37:58 +0100

    Cleanup test

--- src/test/scala/fr/damienraymond/sit/domain/model/change/ChangeSpec.scala
+++ src/test/scala/fr/damienraymond/sit/domain/model/change/ChangeSpec.scala
@@ -10,44 +10,43 @@ class ChangeSpec extends AnyFlatSpec with Matchers {
 
   behavior of "Change"
 
-//
-//  it should "insert a line and shift content" in {
-//    val changes = List(
-//      Change(LinesRemoved.empty, LinesAdded(0 -> "hello", 1 -> "world")),
-//      Change(LinesRemoved.empty, LinesAdded(1 -> "bonjour"))
-//    )
-//
-//    Change.applyChanges(changes).asSortedMap should be (SortedMap(
-//      0 -> "hello",
-//      1 -> "bonjour",
-//      2 -> "world"
-//    ))
-//  }
-//
-//  it should "insert a line at the end of the file" in {
-//    val changes = List(
-//      Change(LinesRemoved.empty, LinesAdded(0 -> "hello", 1 -> "world")),
-//      Change(LinesRemoved.empty, LinesAdded(2 -> "bonjour"))
-//    )
-//
-//    Change.applyChanges(changes).asSortedMap should be (SortedMap(
-//      0 -> "hello",
-//      1 -> "world",
-//      2 -> "bonjour"
-//    ))
-//  }
-//
-//  it should "replace a line" in {
-//    val changes = List(
-//      Change(LinesRemoved.empty, LinesAdded(0 -> "hello", 1 -> "world")),
-//      Change(LinesRemoved(1), LinesAdded(1 -> "bonjour"))
-//    )
-//
-//    Change.applyChanges(changes).asSortedMap should be (SortedMap(
-//      0 -> "hello",
-//      1 -> "bonjour"
-//    ))
-//  }
+  it should "insert a line and shift content" in {
+    val changes = List(
+      Change(LinesRemoved.empty, LinesAdded(0 -> "hello", 1 -> "world")),
+      Change(LinesRemoved.empty, LinesAdded(1 -> "bonjour"))
+    )
+
+    Change.applyChanges(changes).asSortedMap should be (SortedMap(
+      0 -> "hello",
+      1 -> "bonjour",
+      2 -> "world"
+    ))
+  }
+
+  it should "insert a line at the end of the file" in {
+    val changes = List(
+      Change(LinesRemoved.empty, LinesAdded(0 -> "hello", 1 -> "world")),
+      Change(LinesRemoved.empty, LinesAdded(2 -> "bonjour"))
+    )
+
+    Change.applyChanges(changes).asSortedMap should be (SortedMap(
+      0 -> "hello",
+      1 -> "world",
+      2 -> "bonjour"
+    ))
+  }
+
+  it should "replace a line" in {
+    val changes = List(
+      Change(LinesRemoved.empty, LinesAdded(0 -> "hello", 1 -> "world")),
+      Change(LinesRemoved(1), LinesAdded(1 -> "bonjour"))
+    )
+
+    Change.applyChanges(changes).asSortedMap should be (SortedMap(
+      0 -> "hello",
+      1 -> "bonjour"
+    ))
+  }
 
   it should "remove the right line" in {
     Change.applyChanges(
