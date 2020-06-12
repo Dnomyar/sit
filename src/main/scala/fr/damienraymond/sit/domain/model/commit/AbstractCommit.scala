ee6eac405fc9fb2c5d5205e805cbe5ce3a630a49
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2020-06-12 15:38:49 +0100

    Implement commit hash

--- src/main/scala/fr/damienraymond/sit/domain/model/commit/AbstractCommit.scala
+++ src/main/scala/fr/damienraymond/sit/domain/model/commit/AbstractCommit.scala
@@ -1,14 +1,27 @@
 package fr.damienraymond.sit.domain.model.commit
 
+import java.security.MessageDigest
+
 import fr.damienraymond.sit.domain.model.file.FileChanged
 
 sealed trait AbstractCommit {
-  def hash: CommitHash = CommitHash("todo")
+  def hash: CommitHash
   def changes: Set[FileChanged]
 }
 
-case class OrphanCommit(changes: Set[FileChanged]) extends AbstractCommit
+case class OrphanCommit(changes: Set[FileChanged]) extends AbstractCommit {
+  override def hash: CommitHash = CommitHash(MessageDigest.getInstance("MD5").digest(changes.toString().getBytes()).map("%02X".format(_)).mkString)
+}
 
-case class Commit(parentHash: CommitHash, changes: Set[FileChanged]) extends AbstractCommit
+case class Commit(parentHash: CommitHash, changes: Set[FileChanged]) extends AbstractCommit {
+  override def hash: CommitHash = CommitHash(MessageDigest.getInstance("MD5").digest(
+    (changes.toString() + parentHash.toString).getBytes()
+  ).map("%02X".format(_)).mkString)
 
-case class MergeCommit(parentHashes: List[CommitHash], changes: Set[FileChanged]) extends AbstractCommit
+}
+
+case class MergeCommit(parentHashes: List[CommitHash], changes: Set[FileChanged]) extends AbstractCommit {
+  override def hash: CommitHash = CommitHash(MessageDigest.getInstance("MD5").digest(
+    (changes.toString() + parentHashes.toString).getBytes()
+  ).map("%02X".format(_)).mkString)
+}
